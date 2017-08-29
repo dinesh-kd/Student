d763137d0a10d22b06acc079cda6a5aeb7be4189
Dinesh <dinesh.devaraj@infinijith.com>
2017-08-29 11:16:08 +0530

    header ui

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MdToolbarModule } from '@angular/material';
 
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MdToolbarModule
   ],
   declarations: [HeaderComponent, SidebarComponent],
   exports:[HeaderComponent, SidebarComponent]
