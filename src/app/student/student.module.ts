68abc3bc5b8be3afeb3fb53e3755d6694e9642b8
Dinesh <dinesh.devaraj@infinijith.com>
2017-08-29 11:22:50 +0530

    student components

--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StudentListComponent } from './student-list/student-list.component';
+import { StudentAddComponent } from './student-add/student-add.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [StudentListComponent, StudentAddComponent]
 })
 export class StudentModule { }
