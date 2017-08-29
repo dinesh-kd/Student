d325cc7c48f3140e2f2b3d1eac04e63c03d5657f
Dinesh <dinesh.devaraj@infinijith.com>
2017-08-29 11:13:57 +0530

    header and sidebar component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [HeaderComponent, SidebarComponent],
+  exports:[HeaderComponent, SidebarComponent]
 })
 export class CoreModule { }
